e6f5e87562deda4705e4ac6d434602d2c6344f94
Alexander Gruzintsev <overtired@yandex-team.ru>
2019-04-01 22:42:36 +0300

    Fixed build script

--- build.sh
+++
@@ -1,9 +0,0 @@
-cd C:\Users\polin\IdeaProjects\KIK-Java
-
-jar cf ./out/library/libSum.jar ./src/ru/polinaegubova/summator/Summator.java
-
-mkdir C:\Users\polin\IdeaProjects\KIK-Java\out\app
-
-javac -sourcepath .  -d out/app ./src/ru/polinaegubova/summator/*.java
-
-java -cp ./out/app ru.polinaegubova.summator.Main 1 2
---
+++ build_and_run.sh
@@ -0,0 +1,20 @@
+# Делаем директорию, в которой находится этот скрипт текущей
+cd "$( cd "$(dirname "$0")" ; pwd -P )"
+
+# Создаем директории для .class файлов
+mkdir -p out/app
+mkdir -p out/library
+
+# Собираем библиотеку
+javac -sourcepath src -d out/library src/ru/polinaegubova/summator/Summator.java
+
+# Упаковываем библиотеку в jar
+jar -cf out/library/libSum.jar -C out/library ru/polinaegubova/summator/Summator.class
+
+# Собираем основное приложение
+javac -sourcepath src  -d out/app src/ru/polinaegubova/summator/Main.java
+
+# Запускаем приложение, указывая путь к скомпилированному главному приложению out/app
+# и к скомпилированной библиотеке, которая была упакована в jar
+java -cp out/app:out/library/libSum.jar ru.polinaegubova.summator.Main 1 2
+
