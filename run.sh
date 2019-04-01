d72a4dd98e15edb2dc7048e7d5c8debd71db947e
polinaegubova <polinaegubova@mail.ru>
2019-04-01 11:34:16 +0300

    new lines

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ cd C:\Users\polin\IdeaProjects\KIK-Java
 
 mkdir bin
 
-javac -sourcepath src  -d bin ./src/ru/polinaegubova/summator/*.java
+javac -sourcepath src -d bin ./src/ru/polinaegubova/summator/*.java
 
 java -cp ./bin ru.polinaegubova.summator.Main 1 2
