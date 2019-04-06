e6bc8db2ef2ad28e7bf20ada26deade0753371b2
polinaegubova <polinaegubova@mail.ru>
2019-04-06 15:00:15 +0300

    add method

--- src/ru/polinaegubova/reflection/Main.java
+++ src/ru/polinaegubova/reflection/Main.java
@@ -1,6 +1,8 @@
 package ru.polinaegubova.reflection;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class Main {
 
@@ -9,9 +11,10 @@ public class Main {
         private int value;
         MyClass() { value = 100; }
         public void setValue(int value) {this.value = value; }
+        private void printHello() { value = 10; }
     }
 
-    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
 
         MyClass myClass = new MyClass();
 
@@ -31,8 +34,27 @@ public class Main {
 
         long time3 = System.currentTimeMillis();
 
-        System.out.println("Start time (ms): " + time1);
+        for (int i = 0; i < 2000000; i++) {
+            myClass.printHello();
+        }
+
+        long time4 = System.currentTimeMillis();
+
+        final Method method = myClass.getClass().getDeclaredMethod("printHello");
+        method.setAccessible(true);
+        for (int i = 0; i < 2000000; i++) {
+            method.invoke(myClass);
+        }
+
+        long time5 = System.currentTimeMillis();
+
+        System.out.println("Start time for setter (ms): " + time1);
         System.out.println("Time after setValue (ms): " + time2);
         System.out.println("Time after reflection (ms): " + time3);
+        System.out.println();
+        System.out.println("Start time for method (ms): " + time3);
+        System.out.println("Time after execute metod (ms): " + time4);
+        System.out.println("Time after reflection (ms): " + time5);
+
     }
 }
